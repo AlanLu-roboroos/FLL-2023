45a7d7c380ab7e2fb09d634a17e313c5a257fc13
alanlu <alan.lu@roboroos.org.au>
2023-12-11 19:50:24 +1030

    Chicken run done

--- runs/chickenRun.py
+++ runs/chickenRun.py
@@ -5,9 +5,17 @@ class ChickenRun(Thread):
     def __init__(self, config):
         self.config = config
         self.drive = config.drive
+        self.spinner = config.LMmotor
         self.wait = config.timer.wait
 
     def run(self):
-        self.drive.setHead(90)
+        self.drive.setHead(-45)
+
+        self.drive.moveDist(400, heading=-45)
+        self.spinner.run(-4000)
+        self.wait(5000)
+        self.drive.moveDist(-50)
+        self.drive.moveArc(100, -90, speed=-100)
+        self.drive.moveDist(-400, heading=-80, turn=False)
 
         self.config.state.setState(1)
