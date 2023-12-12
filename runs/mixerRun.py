32514c60cf68f8b7f653a465c32696cb56145f13
alanlu <alan.lu@roboroos.org.au>
2023-12-12 15:07:38 +1030

    mixerRun worked

--- runs/mixerRun.py
+++ runs/mixerRun.py
@@ -5,9 +5,37 @@ class MixerRun(Thread):
     def __init__(self, config):
         self.config = config
         self.drive = config.drive
+        self.arm = config.LMmotor
         self.wait = config.timer.wait
 
     def run(self):
-        self.drive.setHead(90)
+        self.drive.setHead(0)
+        self.drive.moveDist(720, heading=15, turn=False)
+        self.drive.turnTo(45)
+        Thread(target=self.dropFigures).start()
+        self.wait(1000)
+        self.drive.moveDist(-80, heading=45)
+        self.drive.turnTo(135)
+        self.drive.moveDist(-100)
+        self.drive.moveDist(30)
+        self.drive.moveDist(-30)
+        # self.drive.moveDist(30)
+        self.drive.moveDist(520, heading=-170, turn=False)
+        Thread(target=self.dropArm).start()
+        self.drive.spinTo(45)
+        self.drive.moveDist(57)
+        # Thread(target=self.drive.moveDist, args=[10]).start()
+        self.arm.run_angle(800, 1200)
+        self.wait(500)
+        self.drive.turnTo(63)
+        self.drive.moveDist(80)
+        self.arm.run_angle(-800, 1200)
+        self.drive.moveDist(-400, down=False)
 
         self.config.state.setState(1)
+
+    def dropFigures(self):
+        self.arm.run_angle(800, 1600)
+
+    def dropArm(self):
+        self.arm.run_angle(-800, 1600)
